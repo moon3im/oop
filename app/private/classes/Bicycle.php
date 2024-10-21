66cff5af99a0218ee6b28bf2b7883ac4dcf9188a
AbdelmonaimKH <akhennouf@doubleclicktech.dz>
2024-10-21 17:26:04 +0100

    feat : db oop course start

--- app/private/classes/Bicycle.php
+++ app/private/classes/Bicycle.php
@@ -13,6 +13,7 @@ class Bicycle {
     public $color;
     public $price;
     public $condition;
+    public $category;
     public $name;
     
     
@@ -20,15 +21,28 @@ class Bicycle {
       $this->brand = $args['brand'] ?? null;
       $this->model = $args['model'] ?? null ;
       $this->year = $args['year'] ?? null ;
-      $this->description = $args['description'] ?? null ;
       $this->color = $args['color'] ?? null ;
-      $this->price = $args['price'] ?? null ;
       $this->weight = $args['weight'] ?? null ;
+      $this->price = $args['price'] ?? null ;
+      $this->description = $args['description'] ?? null ;
+      $this->category = $args['category'] ?? null ;
       $this->condition = $args['condition'] ?? null ;
       $this->name = $this->brand . '' . $this->year . '' . $this->model ;
     }
 
-
+    public static function intstantiate_object_from_csv($bicycle_data){
+        return new static([
+          'brand' => $bicycle_data[0],
+          'model' => $bicycle_data[1],
+          'year' => $bicycle_data[2],
+          'color' => $bicycle_data[3],
+          'weight' => $bicycle_data[4],
+          'price' => $bicycle_data[5],
+          'description' => $bicycle_data[6],
+          'category' => $bicycle_data[7],
+          'condition' => $bicycle_data[8],
+        ]);
+    }
     public static function set_weight($weight){
 
       if (is_null($weight)){
--- app/public/src/inventory.php
+++ app/public/src/inventory.php
@@ -17,15 +17,18 @@ use App\Bicycle;
 use Classes\ElectricalBicycle;
 use function App\private\dd;
 
-$bicycles= new Bicycle(['brand' => 'Speedster' , 'model' => 'VTT' , 'year' => 2022 ,'color' => 'Red', 'weight' => 85.26 , 'price' => '150.00' ,'description' => 'A lightweight hybrid bike for city','condition' => 'good']);
+// $bicycles= new Bicycle(['brand' => 'Speedster' , 'model' => 'VTT' , 'year' => 2022 ,'color' => 'Red', 'weight' => 85.26 , 'price' => '150.00' ,'description' => 'A lightweight hybrid bike for city','condition' => 'good']);
 
-
+    
 $file = fopen("../../private/data.csv","r");
 $data = fgetcsv($file, 1000, ",", '"', "\\");
-dd($data); 
+
+$bicycles = [];
+while($bicycle = fgetcsv($file, 1000, ",", '"', "\\")){ 
+    array_push($bicycles,Bicycle::intstantiate_object_from_csv($bicycle));
+}
 ?>
 <body class="bg-gray-100">  
-
     <header class="bg-blue-600 text-white py-4">
         <div class="container mx-auto text-center">
             <h1 class="text-3xl font-bold">Bicycle Inventory</h1>
@@ -41,7 +44,7 @@ dd($data);
     </a>
 </div>
 
-        <table class="bg-white border border-gray-300 w-full">
+        <table class="bg-white border border-gray-300 w-full mb-8">
          <thead>
         <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
             <th class="py-3 px-6 text-left">Brand</th>
@@ -55,34 +58,24 @@ dd($data);
         </tr>
     </thead>
     <tbody class="text-gray-600 text-sm font-light">
-    <?php if(isset($_POST)): ?>
-        <tr class="border-b border-gray-200 hover:bg-gray-100">
-                <td class="py-3 px-6"><?php echo $_POST['brand'] ?? null; ?></td>
-                <td class="py-3 px-6"><?php echo $_POST['model']?? null; ?></td>
-                <td class="py-3 px-6"><?php echo $_POST['year'] ?? null; ?></td>
-                <td class="py-3 px-6"><?php echo $_POST['color']?? null; ?></td>
-                <td class="py-3 px-6"><?php echo $_POST['weight'] ?? null; ?></td>
-                <!-- <td class="py-3 px-6"><?php echo $_POST['price'] ?? null; ?></td> -->
-                <td class="py-3 px-6"><?php echo $_POST['description'] ?? null; ?></td>
-                <td class="py-3 px-6"><?php echo $_POST['condition'] ?? null; ?></td>
-            </tr>
-            <?php endif; ?>
     <?php if (!empty($bicycles)): ?>
-                <tr class="border-b border-gray-200 hover:bg-gray-100">
-                    <td class="py-3 px-6"><?php echo $bicycles->brand; ?></td>
-                <td class="py-3 px-6"><?php echo $bicycles->model; ?></td>
-                <td class="py-3 px-6"><?php echo $bicycles->year; ?></td>
-                <td class="py-3 px-6"><?php echo $bicycles->color; ?></td>
-                <td class="py-3 px-6"><?php echo $bicycles->weight; ?></td>
-                <td class="py-3 px-6"><?php echo '$' . number_format($bicycles->price, 2); ?></td>
-                <td class="py-3 px-6"><?php echo $bicycles->description; ?></td>
-                <td class="py-3 px-6"><?php echo $bicycles->    condition; ?></td>
-            </tr>
-    <?php else: ?>
-        <tr>
-            <td colspan="8" class="py-3 px-6 text-center">No records found.</td>
-        </tr>
-    <?php endif; ?>
+                <?php foreach ($bicycles as $bicycle): ?>
+                    <tr class="border-b border-gray-200 hover:bg-gray-100">
+                        <td class="py-3 px-6"><?php echo $bicycle->brand; ?></td>
+                        <td class="py-3 px-6"><?php echo $bicycle->model; ?></td>
+                        <td class="py-3 px-6"><?php echo $bicycle->year; ?></td>
+                        <td class="py-3 px-6"><?php echo $bicycle->color; ?></td>
+                        <td class="py-3 px-6"><?php echo $bicycle->weight; ?></td>
+                        <td class="py-3 px-6"><?php echo '$' . $bicycle->price; ?></td>
+                        <td class="py-3 px-6"><?php echo $bicycle->description; ?></td>
+                        <td class="py-3 px-6"><?php echo $bicycle->condition; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="8" class="py-3 px-6 text-center">No records found.</td>
+                </tr>
+            <?php endif; ?>
     </tbody>
 </table>
 
